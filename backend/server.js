71fc987145b595c40ae1b9108705e7c6de909df9
Naga Sai <108686878+saijanipireddy@users.noreply.github.com>
2025-02-03 07:09:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,15 @@ const port = 4000
 
 // middleware 
 
-app.use(express.json())
+
 app.use(cors(
-    
-))
+    {
+        origin: ["https://deploy-mern-1whq.vercel.app"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
+app.use(express.json())
 
 // DB Connection 
 connectDB();
